735e7d5f0e519a2ff67bcf40ef329f1fa4e7b137
Prince <princekarabo70@gmail.com>
2022-11-11 22:45:02 +0200

    Create number.py

---
+++ Python/number.py
@@ -0,0 +1,14 @@
+# print(4 + 5)
+
+a = 4
+a += 2
+
+b = int(a /3)
+
+print('a type is', type(a))
+print('a = ', a)
+
+print('b type is', type(b))
+print('b = ', b)
+
+print('b type is now ', type(b))
